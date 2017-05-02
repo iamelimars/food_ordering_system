eb63b5074d9f34fa10872f3dbfa9fbc4e9ef2f70
Eli MArshall <elimarshall@Elis-Mac-mini.local>
2017-05-02 01:41:35 -0400

    Added alternating design to menu section

--- app/views/home.php
+++ app/views/home.php
@@ -1,7 +1,36 @@
+
+<?php require VIEW_ROOT . '/templates/header.php' ?>
+<?php $i = 0; ?>
 <?php foreach ($menu as $item): ?>
-  <h4><?php echo escape($item['title']); ?></h4>
-  <h4><?php echo escape($item['description']); ?></h4>
-  <h4><?php echo escape($item['price']); ?></h4>
-  <h4><?php echo escape($item['preptime']); ?></h4>
-  <h4><?php echo escape($item['img-url']); ?></h4>
+  <?php $i++; ?>
+  <?php if($i % 2 == 0): ?>
+    <div id="menu-section" class="col-sm-12">
+      <div class="col-sm-6 container" id="menu-content">
+        <h4><?php echo escape($item['title']); ?></h4>
+        <h4><?php echo escape($item['description']); ?></h4>
+        <h4><?php echo escape($item['price']); ?></h4>
+        <h4><?php echo escape($item['preptime']); ?></h4>
+      </div>
+      <div class="col-sm-6" id="menu-img-div">
+        <img id="menu-img" src="<?php echo escape($item['img-url']); ?>" alt="">
+      </div>
+    </div>
+  <?php else: ?>
+    <div id="menu-section" class="col-sm-12">
+      <div class="col-sm-6" id="menu-img-div">
+        <img id="menu-img" src="<?php echo escape($item['img-url']); ?>" alt="">
+      </div>
+      <div class="col-sm-6 container" id="menu-content">
+        <h4><?php echo escape($item['title']); ?></h4>
+        <h4><?php echo escape($item['description']); ?></h4>
+        <h4><?php echo escape($item['price']); ?></h4>
+        <h4><?php echo escape($item['preptime']); ?></h4>
+      </div>
+    </div>
+  <?php endif; ?>
+
+
+
 <?php endforeach; ?>
+
+<?php require VIEW_ROOT . '/templates/footer.php' ?>
---
+++ app/views/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/styles.css">
+  </head>
+  <body>
